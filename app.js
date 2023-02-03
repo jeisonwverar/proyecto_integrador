6049320c57728637a29c00ebabc46edb35085f8f
Jeison Vera <jw.vera.r@gmail.com>
2023-02-03 12:41:27 -0500

    restructuracion de views con ejs ingreso de favicon y ruta 404

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 import  Express  from "express";
-import morgan from "morgan";
 import  Morgan  from "morgan";
 import {dirname,join} from "path";
-//import {fileURLToPath } from "url";
-//import indexRoutes from "./routes/index-router.js";
-
-const app= Express();
+import {fileURLToPath } from "url";
 const port=process.env.PORT||4040;
+
+//uso de dirname en ECMAScript 6
+const __dirname=dirname(fileURLToPath(import.meta.url));
+const app= Express();
+
+app.set('views',join(__dirname,'views'));
+app.set('view engine','ejs');
+//archivos estaticos
+app.use(Express.static(join(__dirname ,'public')));
+
+app.get('/',(req,res)=>{
+    res.render('home',{
+        title:'home'
+    })
+})
+app.get('/alta',(req,res)=>{
+    res.render('alta',{
+        title:'alta'
+    })
+})
+app.get('/contacto',(req,res)=>{
+    res.render('contacto',{
+        title:'contacto'
+    })
+})
+app.get('/nosotros',(req,res)=>{
+    res.render('nosotros',{
+        title:'nosotros'
+    })
+})
+app.get('/*',(req,res)=>{
+    res.render('error404',{
+        title:'404'
+    })
+})
  //middleware
 app.use(Morgan('dev'));
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
